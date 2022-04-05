0df3f35a5450f630dd5959c53e3c4d8db240c468
정서연 <jjungseoyeon@naver.com>
2022-04-06 01:45:26 +0900

    패스워드 암호화 구현

--- src/main/java/com/milkyway/dreamform/service/UserService.java
+++ src/main/java/com/milkyway/dreamform/service/UserService.java
@@ -4,27 +4,32 @@ import com.milkyway.dreamform.dto.SignupRequestDto;
 import com.milkyway.dreamform.model.User;
 import com.milkyway.dreamform.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class UserService {
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void registerUser(SignupRequestDto requestDto) {
         String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
         // 회원 ID 중복 확인
         Optional<User> found = userRepository.findByUsername(username);
         if (found.isPresent()) {
             throw new IllegalArgumentException("중복된 사용자 ID 가 존재합니다.");
         }
+
+        // 패스워드 인코딩
+        String password = passwordEncoder.encode(requestDto.getPassword());
 
         String nickname = requestDto.getNickname();
         String email = requestDto.getEmail();
