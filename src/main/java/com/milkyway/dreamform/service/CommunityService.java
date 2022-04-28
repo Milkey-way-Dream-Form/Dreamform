92f60b754a03d0c94e8b23122f99f7468081eb70
choijaehak <cms893811@naver.com>
2022-04-28 22:32:31 +0900

    application.properties profile and community function coding

---
+++ src/main/java/com/milkyway/dreamform/service/CommunityService.java
@@ -0,0 +1,12 @@
+//package com.milkyway.dreamform.service;
+//
+//import com.milkyway.dreamform.repository.CommunityRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class CommunityService {
+//    @Autowired
+//    private CommunityRepository communityRepository;
+//
+//}
