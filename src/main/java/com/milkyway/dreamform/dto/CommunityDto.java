2f54dc1c151ad036e866d5279d114d899bf7f899
choijaehak <cms893811@naver.com>
2022-05-11 23:30:16 +0900

    Community commit
    Community commit

--- src/main/java/com/milkyway/dreamform/dto/CommunityDto.java
+++ src/main/java/com/milkyway/dreamform/dto/CommunityDto.java
@@ -5,10 +5,12 @@ import com.milkyway.dreamform.model.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.domain.Page;
 
 import java.time.LocalDateTime;
 
+@Setter
 @Getter
 @NoArgsConstructor
 public class CommunityDto {
