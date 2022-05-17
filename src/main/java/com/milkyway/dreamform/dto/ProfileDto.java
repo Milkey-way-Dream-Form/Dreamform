954a25a2c04f41185b0efb9a59fc08a4526d411d
JungGon Lee <dlwndrhs05@gmail.com>
2022-05-18 02:46:51 +0900

    profile

---
+++ src/main/java/com/milkyway/dreamform/dto/ProfileDto.java
@@ -0,0 +1,12 @@
+package com.milkyway.dreamform.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ProfileDto {
+    private String username;
+    private String password;
+    private String email;
+}
---
+++ src/main/java/com/milkyway/dreamform/service/ProfileService.java
@@ -0,0 +1,7 @@
+package com.milkyway.dreamform.service;
+
+public class ProfileService {
+//    public class save(){
+//        프로필 변경후 저장
+//    }
+}
