8f67a5e499ec435f74c8f59bfb143c8a9d3bf848
JungGon Lee <dlwndrhs05@gmail.com>
2022-05-18 10:43:26 +0900

    up date profile

--- src/main/java/com/milkyway/dreamform/controller/ProfileController.java
+++ src/main/java/com/milkyway/dreamform/controller/ProfileController.java
@@ -1,12 +1,18 @@
 package com.milkyway.dreamform.controller;
 
+import com.milkyway.dreamform.security.UserDetailsImpl;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProfileController {
     @GetMapping("/profile")
-    public String profile(){
+    public String profile(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        model.addAttribute("username", userDetails.getUsername());
+        model.addAttribute("email",userDetails.getEmial());
+
         return "profile";
     }
 
